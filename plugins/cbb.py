004c47bd1999aee1d5297e2c64023837302d679e
Project999x <105520683+Project999x@users.noreply.github.com>
2025-03-04 14:17:32 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,121 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"""<b><blockquote>╭───────────⍟
+├➤ ᴏᴡɴᴇʀ : <a href='t.me/WhiteBeard_Sama'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>
+├➤ ʟɪʙʀᴀʀy : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a>
+├➤ ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a>
+├➤ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href=https://t.me/Shirohige_Animes>ᴀɴɪᴍᴇ ᴅʀɪꜰᴛ</a>
+├➤ ᴘᴀɪᴅ ʙᴏᴛ : <a href=https://t.me/WhiteBeard_Sama>ᴍʏꜱᴛɪᴄ ɴᴇ𝐱ᴜꜱ 𓄀</a>
+├➤ ᴅᴇᴠʟᴏᴘᴇʀ : <a href=https://t.me/WhiteBeard_Sama>ᴍʏꜱᴛɪᴄ ɴᴇ𝐱ᴜꜱ 𓄀</a>
+╰───────────────⍟</blockquote></b>""",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [ [ InlineKeyboardButton("sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", callback_data ="source"),
+                  InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ" , callback_data = "main")],
+                 [InlineKeyboardButton("ᴡᴀᴛᴄʜ sʜᴏʀᴛs 𝟷𝟾+ ᴠɪᴅᴇᴏs", url = "https://t.me/Dsh_Alliance/shorts")],
+                    [
+                        InlineKeyboardButton("🔙 ʙᴀᴄᴋ ", callback_data = "home"),
+                        InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+       
+
+        
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+    elif data == "main":
+        await query.message.edit_text(
+            text=f"<blockquote>ʜᴇʟʟᴏ ᴍʏ ᴜsᴇʀs ᴍʏ ᴜᴘᴅᴀᴛᴇ & ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ ɪs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.</blockquote>",
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup(
+                [   
+                    [
+                        InlineKeyboardButton("ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ", url="https://t.me/Dsh_Alliance"),
+                        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ",url = "t.me/Shirohige_Animes")
+                    ],
+                    [   InlineKeyboardButton("🔙 ʙᴀᴄᴋ ", callback_data = "home"), 
+                        InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except: 
+            pass
+    elif data == "home":
+        await query.message.edit_text(
+            text=f"<b><blockquote>ʜᴇʏ !! {query.from_user.mention}\n\nɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>",
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [ InlineKeyboardButton(text="🏖️", callback_data="about"),
+                    InlineKeyboardButton(text="🍂", callback_data="about"),
+                    InlineKeyboardButton(text="⚠️", callback_data="me"),
+                    InlineKeyboardButton(text="💸", callback_data="about"),
+                    InlineKeyboardButton(text="🎭", callback_data="about"),
+                ],[ InlineKeyboardButton( "ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ", callback_data = "main" ),
+                    InlineKeyboardButton("sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ", callback_data = "source")
+                ], [ InlineKeyboardButton("ᴡᴀᴛᴄʜ 𝟷𝟾+ sʜᴏʀᴛs ᴠɪᴅᴇᴏs", url = "https://t.me/Dsh_Alliancet/shorts") ],
+                [
+                    InlineKeyboardButton("🤖 ᴀʙᴏᴜᴛ ᴍᴇ", callback_data = "about"),
+                    InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
+                ]
+            ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except: 
+            pass
+    
+    elif data == "me":
+            await query.message.edit(
+                text=f"<b>ᴛʜɪs sᴇᴄᴛɪᴏɴ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ᴏɴʟʏ ғᴏʀ ᴀᴅᴍɪɴs & ᴅᴇᴠᴇʟᴏᴘᴇʀ</b>",
+                disable_web_page_preview=True,
+                reply_markup = InlineKeyboardMarkup(
+                    [
+                        [  InlineKeyboardButton("ᴅᴇᴠʟᴏᴘᴇʀ",url= "t.me/WhiteBeard_Sama"),
+                         InlineKeyboardButton("ᴀᴅᴍɪɴ",url = "t.me/Aizen_Sausake_Sama")],
+                        [ InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data = "home"),
+                         InlineKeyboardButton( "🚫 ᴄʟᴏsᴇ", callback_data = "close")]
+                    ]
+                )
+         )
+
+    elif data == "source":
+        await query.message.edit_text(
+            text=f"<b><blockquote>ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ\nɪɴ ᴛᴡᴏ ᴡᴀʏs\n★ <a herf='https://publicearn.site/OFxzQIX'>ɢɪᴛʜᴜʙ</a> \n★ <a herf='https://t.me/+Yw4K5nZyPyIzNjg1'>ᴢɪᴘ ғɪʟᴇ </a></blockquote></b>",
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup(
+                [   
+                    [
+                        InlineKeyboardButton("ɢɪᴛʜᴜʙ ", url="https://publicearn.site/OFxzQIX"),
+                        InlineKeyboardButton("ᴢɪᴘ ғɪʟᴇ",url="https://t.me/+R5jbFbY1nuhmMDk1")
+                    ],
+                    [   InlineKeyboardButton("🔙 ʙᴀᴄᴋ" , callback_data = "home"),
+                        InlineKeyboardButton("🚫 ᴄʟᴏsᴇ ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
